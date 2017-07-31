3896a8413b4e6f0448487ab6fac408a58bebcb6e
Brad Aylsworth <brad.aylsworth@gmail.com>
2017-07-30 22:12:30 -0500

    actually add file

---
+++ malapi.js
@@ -0,0 +1,12 @@
+funciton getUserList() {
+    var malName = document.forms["mainForm"]["malName"].value;
+
+    // var xhr = new XMLHttpRequest();
+    // var requestUrl = "http://myanimelist.net/malappinfo.php?u=" + malName + "&status=all&type=anime";
+    // xhr.open("GET", requestURL, false);
+    // xhr.send();
+
+    var userListDisplay = document.getElementById("userList");
+    //   userListDisplay.innerHTML = xhr.responseText;
+    userListDisplay.innerHTML = malName;
+}
