da806e49b4711a2d5eacb3194ecf1d8592235420
Brad Aylsworth <brad.aylsworth@gmail.com>
2017-07-31 03:31:36 -0500

    complicated attempts to debug the CGI problems

--- cgi-bin/malmirror.py
+++ cgi-bin/malmirror.py
@@ -4,23 +4,35 @@ import cgi
 #import cgitb
 #cgitb.enable() # special exception handler (that just keeps me from running it in cli)
 
+import urllib.request
+
 ###### HTTP headers
-print("Content-Type: application/xml; charset=UTF-8")
+#print("Content-Type: application/xml; charset=UTF-8")
+print("Content-Type: text/html")
 print("access-control-allow-origin: *")
 
 print()
 
 ###### XML proxy
-form = cgi.FieldStorage()
-
+#form = cgi.FieldStorage()
 #form = {'u':'test', 'status':'some', 'type':'clacks'}
+form = {'u':'SkeevingQuack', 'status':'all', 'type':'anime'}
+
 querymap = map(lambda key:key+'='+form[key], form.keys())
-queries = '&amp;'.join(querymap)
-#queries = '&'.join(querymap)
+queries = '&'.join(querymap)
 
 base_url = "http://myanimelist.net/malappinfo.php"
-#queries = "u=FoxInFlame&status=all&type=anime"
 
-print("<query>")
-print(base_url + '?' + queries)
-print("</query>")
+request = base_url + '?' + queries
+
+with urllib.request.urlopen(request) as response:
+#    print(response.read().decode('utf-8', errors='replace'))
+    import xml.etree.ElementTree as ET
+    root = ET.fromstring(response.read().decode('utf-8'))
+    with open("list.xml", 'w') as f:
+        print("<root>")
+        try:
+            print(f.write(ET.tostring(root, encoding="unicode")))
+        except Exception as e:
+            print(e)
+        print("</root>")
